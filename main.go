f9b87ae845937beea757098e959008f5730cde23
pooriaghaedi <pooria.ghaedi@gmail.com>
2023-07-15 07:05:11 +0200

    Improving the code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 
 	"gopkg.in/yaml.v3"
@@ -11,23 +12,32 @@ import (
 func main() {
 
 	chartPath := os.Getenv("CHART_PATH")
+	imageTag := os.Getenv("IMAGE_TAG")
+
 	data, err := ioutil.ReadFile(chartPath)
 	if err != nil {
-		fmt.Println("Chart Not found", err)
+		log.Fatalf("Error reading chart: %v", err)
 	}
 
 	var valuesData map[string]interface{}
-
 	err = yaml.Unmarshal(data, &valuesData)
 	if err != nil {
-		fmt.Println("JSON ERROR", err)
+		log.Fatalf("Error unmarshalling JSON: %v", err)
 	}
-	valuesData["image"].(map[string]interface{})["tag"] = os.Getenv("IMAGE_TAG")
+
+	// Ensure that "image" key exists and is a map
+	imageData, ok := valuesData["image"].(map[string]interface{})
+	if !ok {
+		log.Fatalf("'image' key is missing or is not a map")
+	}
+
+	imageData["tag"] = imageTag
 
 	modifiedData, err := yaml.Marshal(valuesData)
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	fmt.Println(string(modifiedData))
 	err = ioutil.WriteFile(chartPath, modifiedData, 0644)
 	if err != nil {
